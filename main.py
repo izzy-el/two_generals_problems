ae802f2f1a2fd12c2e5d167f5a10bec00e829502
Smoow <pedrohcm5@gmail.com>
2020-10-22 00:58:17 -0300

    Final v1.0

--- main.py
+++ main.py
@@ -36,34 +36,120 @@ import random
 import time
 from datetime import datetime
 
-# print(random.randint(0, 10))
-# Duração (60-70) 
-# Chance de ser pego no meio - 45%
-# Chance do azul impossibilitar ataque - 1%
-# Vermelho  => 5 mensageiros
-# Azul      => 10 mensageiros
-
 def two_generals():
     sinalizador = 0
-    vermelho_mensageiro = 5
-    azul_mensageiro = 10
     time_init = datetime.now()
     print("Hora de inicio: "+str(time_init.hour)+':'+str(time_init.minute)+":"+str(time_init.second))
-
+    tempoInicio = dateTime_toSeg(time_init.hour, time_init.minute, time_init.second)
+    vermelho_mensageiro = 0
+    azul_mensageiro = 0
     time_elapsed = []
-    
+    tempoVermelho = []
+    tempoAzul = []
+    everReached = 0
     while(True):
-        # Problema em si
-        vermelho_mensageiro -= 1
-        break
+        if (verMensageiro(vermelho_mensageiro, azul_mensageiro) != 0):
+            return verMensageiro(vermelho_mensageiro, azul_mensageiro), time_elapsed
+        if (tempoVermelho == []):
+            # Envia um mensageiro vermelho e verifica se foi capturado
+            time_elapsed, vermelho_mensageiro, tempoVermelho = enviaVermelho(time_elapsed, vermelho_mensageiro, tempoVermelho)
+            capturou, time_elapsed, tempoVermelho, tempoAzul = tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul, 0)
+        if (capturou == 0):
+            # Chega no Azul
+            imp, time_elapsed, azul_mensageiro = chegaNoAzul(time_elapsed, azul_mensageiro)
+            if (imp == 0):
+                everReached = 1
+        if (everReached == 1):
+            if (tempoAzul == []):
+                # Envia um mensageiro azul e verifica se foi capturado
+                time_elapsed, azul_mensageiro, tempoAzul = enviaAzul(time_elapsed, azul_mensageiro, tempoAzul)
+                capt, time_elapsed, tempoVermelho, tempoAzul = tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul, 1)
+            if (capt == 0):
+                return 0, time_elapsed
+        
+            if (capt == 1 or tempoAzul == [70,70]):
+                tempoAzul = []
+        if (capturou == 1 or tempoVermelho == [70,70]):
+            tempoVermelho = []
+        if (vermelho_mensageiro == 5 and azul_mensageiro == 0):
+            return 1, time_elapsed
 
 
-problem = two_generals()
+def verMensageiro(vermelho_mensageiro, azul_mensageiro):
+    if (azul_mensageiro == 10):
+        return 2
+    return 0
 
-# if problem == 0:
-#     # Deu tudo certo
-# if problem == 1:
-#     # Vermelho sem mensageiros
-# if problem == 2:
-#     # Azul sem mensageiros
-# # ...
+
+def enviaVermelho(time_elapsed, vermelho_mensageiro, tempoVermelho):
+    # Enviando o vermelho
+    if (vermelho_mensageiro == 5):
+        return time_elapsed, vermelho_mensageiro, tempoVermelho
+    vermelho_mensageiro += 1
+    time_elapsed.append(random.randint(3600, 4201))
+    print("Enviando mensageiro vermelho", 5-vermelho_mensageiro,"restantes")
+    return time_elapsed, vermelho_mensageiro, tempoVermelho
+
+
+def tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul ,azul):
+    tentativa = random.randint(1, 101)
+    tempoVermelho.append(70)
+    if (azul == 1):
+        tempoAzul.append(70)
+    if (tentativa <= 45):
+        print("Castelo Capturou!")
+        time_elapsed.append(210 * 60)
+        return 1, time_elapsed, tempoVermelho, tempoAzul
+    else:
+        print("Castelo nao Capturou!")
+        return 0, time_elapsed, tempoVermelho, tempoAzul
+
+
+def chegaNoAzul(time_elapsed, azul_mensageiro):
+    negar = random.randint(1, 101)
+    if(negar == 1):
+        print("Exercito azul impossibilitou o horário!")
+        return 1, time_elapsed, azul_mensageiro
+    else:
+        print("Exercito azul aceita o horário!")
+        return 0, time_elapsed, azul_mensageiro
+
+
+def enviaAzul(time_elapsed, azul_mensageiro, tempoAzul):
+    # Enviando o azul
+    azul_mensageiro += 1
+    time_elapsed.append(random.randint(3600, 4201))
+    print("Enviando mensageiro azul", 10-azul_mensageiro,"restantes")
+    return time_elapsed, azul_mensageiro, tempoAzul
+            
+
+def dateTime_toSeg(h, m, s):
+    return s+(m*60)+(h*3600)
+
+def seg_toDateTime(s):
+    s = s % (24 * 3600 * 60)
+    s %= 216000
+    hour = s // 3600
+    s %= 3600
+    minutes = s // 60
+    s %= 60
+    return hour, minutes, s
+
+
+problem, time_elapsed = two_generals()
+if problem == 0:
+    print("\nDeu tudo certo")
+if problem == 1:
+    print("\nVermelho sem mensageiros")
+if problem == 2:
+    print("\nAzul sem mensageiros")
+
+for i in range(0, len(time_elapsed)):
+    try:
+        if (time_elapsed[i+1] == 12600):
+                time_elapsed[i] = -1
+    except:
+        None
+
+hour, minutes, s = seg_toDateTime(sum(time_elapsed))
+print("Tempo decorrido: %02d:%02d:%02d"%(hour, minutes, s))
