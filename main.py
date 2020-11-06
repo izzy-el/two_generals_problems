402b818ec76dc66e6858b62f05526044f9c3a133
Jhones <41956039+JhonesBR@users.noreply.github.com>
2020-11-05 22:56:09 -0300

    Final v2.5

--- main.py
+++ main.py
@@ -34,8 +34,8 @@ um mensageiro e para a possibilidade do exército azul impossibilitar o horário
 
 # Desenvolvido por:
 #   Diogo Silveira dos Santos
-#   João Vitor Izael Souza  
-#   João Vitor Oliveira de Melo  
+#   João Vitor Izael Souza
+#   João Vitor Oliveira de Melo
 #   Luiz Otávio de Oliveira Silva
 #   Pedro Henrique Carreto Morais
 
@@ -43,15 +43,19 @@ import random
 import time
 from datetime import datetime
 
-def two_generals():
-    time_init = datetime.now()
-    print("Hora de inicio: "+str(time_init.hour)+':'+str(time_init.minute)+":"+str(time_init.second))
 
+def two_generals():
+    # Obtenção e exibição do horário do início
+    time_init = datetime.now()
+    print("Hora de inicio: "+str(time_init.hour)+':' + str(time_init.minute)+":"+str(time_init.second))
+
+    # Inicialização de variáveis
     vermelho_mensageiro, azul_mensageiro = 0, 0
-    tempoVermelho, tempoAzul, time_elapsed  = [], [], []
+    tempoVermelho, tempoAzul, time_elapsed = [], [], []
     everReached = 0
 
     while(True):
+        # Verifica se existem mensageiros restantes
         if (verMensageiro(vermelho_mensageiro, azul_mensageiro) != 0):
             return verMensageiro(vermelho_mensageiro, azul_mensageiro), time_elapsed
 
@@ -60,11 +64,11 @@ def two_generals():
             time_elapsed, vermelho_mensageiro, tempoVermelho = enviaVermelho(time_elapsed, vermelho_mensageiro, tempoVermelho)
             capturou, time_elapsed, tempoVermelho, tempoAzul = tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul, 0)
 
-        if (capturou == 0 and everReached != 1):
-            # Chega no Azul
-            imp, time_elapsed, azul_mensageiro = chegaNoAzul(time_elapsed, azul_mensageiro)
-            if (imp == 0):
-                everReached = 1
+            if (capturou == 0):
+                # Chega no Azul
+                imp, time_elapsed, azul_mensageiro = chegaNoAzul(time_elapsed, azul_mensageiro)
+                if (imp == 0):
+                    everReached = 1
 
         if (everReached == 1):
             if (tempoAzul == []):
@@ -72,15 +76,16 @@ def two_generals():
                 time_elapsed, azul_mensageiro, tempoAzul = enviaAzul(time_elapsed, azul_mensageiro, tempoAzul)
                 capt, time_elapsed, tempoVermelho, tempoAzul = tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul, 1)
             if (capt == 0):
+                # Não foi capturado, logo o sinalizador foi disparado
                 return 0, time_elapsed
             if (capt == 1 or sum(tempoAzul) > 4200):
                 tempoAzul = []
 
-        if (capturou == 1 or sum(tempoVermelho) > 12600):
-            everReached = 0
+        if (capturou == 1 or sum(tempoVermelho) > 12600 or everReached == 0):
             tempoVermelho = []
 
 
+# Verifica a existência de mensageiros
 def verMensageiro(vermelho_mensageiro, azul_mensageiro):
     if (vermelho_mensageiro == 5):
         return 1
@@ -91,19 +96,20 @@ def verMensageiro(vermelho_mensageiro, azul_mensageiro):
     return 0
 
 
+# Envia um mensageiro vermelho
 def enviaVermelho(time_elapsed, vermelho_mensageiro, tempoVermelho):
-    # Enviando o vermelho
     if (vermelho_mensageiro == 5):
         return time_elapsed, vermelho_mensageiro, tempoVermelho
 
     vermelho_mensageiro += 1
     time_elapsed.append(random.randint(3600, 4201))
 
-    print("Enviando mensageiro vermelho", 5-vermelho_mensageiro,"restantes")
+    print("Enviando mensageiro vermelho", 5-vermelho_mensageiro, "restantes")
     return time_elapsed, vermelho_mensageiro, tempoVermelho
 
 
-def tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul ,azul):
+# Realiza a verificação randômica de captura de travessia
+def tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul, azul):
     tentativa = random.randint(1, 101)
     tempoDecorrido = random.randint(3600, 4201)
     tempoVermelho.append(tempoDecorrido)
@@ -115,12 +121,12 @@ def tentativaCaptura(time_elapsed, tempoVermelho, tempoAzul ,azul):
         print("Castelo Capturou!")
         time_elapsed.append(12600 + 1)
         return 1, time_elapsed, tempoVermelho, tempoAzul
-
     else:
         print("Castelo nao Capturou!")
         return 0, time_elapsed, tempoVermelho, tempoAzul
 
 
+# Verifica se o azul aceita o horário proposto
 def chegaNoAzul(time_elapsed, azul_mensageiro):
     negar = random.randint(1, 101)
 
@@ -132,15 +138,16 @@ def chegaNoAzul(time_elapsed, azul_mensageiro):
         return 0, time_elapsed, azul_mensageiro
 
 
+# Envia um mensageiro azul
 def enviaAzul(time_elapsed, azul_mensageiro, tempoAzul):
-    # Enviando o azul
     azul_mensageiro += 1
     time_elapsed.append(random.randint(3600, 4201))
 
-    print("Enviando mensageiro azul", 10-azul_mensageiro,"restantes")
+    print("Enviando mensageiro azul", 10-azul_mensageiro, "restantes")
     return time_elapsed, azul_mensageiro, tempoAzul
-            
 
+
+# Transforma segundos para horas, minutos e segundos
 def seg_toDateTime(s):
     s = s % (24 * 3600 * 60)
     s %= 216000
@@ -151,29 +158,36 @@ def seg_toDateTime(s):
     return hour, minutes, s
 
 
-problem, time_elapsed = two_generals()
+for i in range(1, 101):
+    print("\nTentativa", i)
+    # Inicia o problema
+    problem, time_elapsed = two_generals()
 
-if problem == 0:
-    print("\nSinalizador disparado")
+    # Verifica as saidas obtidas
+    if problem == 0:
+        print("\nSinalizador disparado")
 
-if problem == 1:
-    print("\nVermelho sem mensageiros")
+    if problem == 1:
+        print("\nVermelho sem mensageiros")
 
-if problem == 2:
-    print("\nAzul sem mensageiros")
+    if problem == 2:
+        print("\nAzul sem mensageiros")
 
-for i in range(0, len(time_elapsed)):
-    try:
-        if (time_elapsed[i+1] == 12600):
-            time_elapsed[i] = 0
-    except:
-        None
+    # Elimina contagens de tempo inválidas
+    for i in range(0, len(time_elapsed)):
+        try:
+            if (time_elapsed[i+1] == 12600):
+                time_elapsed[i] = 0
+        except:
+            None
 
-time_stamp_inicial = time.time()
-time_stamp_final = time_stamp_inicial + sum(time_elapsed)
+    # Define e exibe os timestamps
+    time_stamp_inicial = time.time()
+    time_stamp_final = time_stamp_inicial + sum(time_elapsed)
 
-hour, minutes, s = seg_toDateTime(sum(time_elapsed))
+    hour, minutes, s = seg_toDateTime(sum(time_elapsed))
 
-print("Tempo decorrido: %02d:%02d:%02d"%(hour, minutes, s))
-print("\nTimestamp Inicial: %.0f"%(time_stamp_inicial))
-print("Timestamp Final: %.0f"%(time_stamp_final))
+    print("Tempo decorrido: %02d:%02d:%02d" % (hour, minutes, s))
+    print("\nTimestamp Inicial: %.0f" % (time_stamp_inicial))
+    print("Timestamp Final: %.0f" % (time_stamp_final))
+    print("\n------------------------------------------------------")
